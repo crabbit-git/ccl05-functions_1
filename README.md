ee42d23e28aa199427f558640a98a26fe2bbf27c
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-20 13:32:11 +0000

    Tidied, added README

---
+++ README.md
@@ -0,0 +1,10 @@
+# What's this then?
+
+This is my solution for a practice exercise assigned as part of a software development course. It  was assigned on the first day of the fifth week (Monday 14th March 2022) but for various reasons I'm only tidying it up and sticking it in a repository at the end of that week.
+
+The "startpoint" directory contains the source files provided with the assignment, and "task.md" contains the instructions we were given. The code I wrote resides in `src/python_functions_test.py` but rather than running that file directly, the point is to instead run `run_tests.py` in the root directory to in turn run `tests/python_functions_test.py`, which runs several predefined tests on my code to ensure that the objectives of the task set have been met.
+
+
+# Right... but what does it do?
+
+Nothing especially exciting, it's just a bunch of simple bits and bobs showing the kinds of things you can do with a function. Some of it is numerical manipulations, some is to do with strings, some is more of the traversals around lists and dictionaries seen in previous labs. Have a poke around in `src/python_functions_test.py` in whatever text editor you like if you're particularly curious for whatever reason.
--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -22,20 +22,33 @@ def join_string(string_1, string_2):
 def add_string_as_number(string_1, string_2):
     return int(string_1) + int(string_2)
 
-months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+# This bit about converting month names to integers could be done more than one way.
+# You could use a list...
+
+# months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+# def number_to_full_month_name(month_number):
+#     return months[month_number - 1]
+
+# def number_to_short_month_name(month_number):
+#     return months[month_number - 1][0:3]
+
+# ... or do it with a dictionary instead:
+
 months = {
-    "1": "January",
-    "2": "February",
-    "3": "March",
-    "4": "April",
-    "5": "May",
-    "6": "June",
-    "7": "July",
-    "8": "August",
-    "9": "September",
-    "10": "October",
-    "11": "November",
-    "12": "December"
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
 }
 
 def number_to_full_month_name(month_number):
